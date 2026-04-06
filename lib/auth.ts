861fc536a7005d670d3311d3eb49e4249e93c011
Mikael Feltenmark <mfeltenmark@gmail.com>
2026-04-06 12:29:52 +0200

    fix: disable pkce for google provider to fix InvalidCheck error

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,6 +19,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      checks: ['state'],
       authorization: {
         params: {
           scope: 'openid email profile https://www.googleapis.com/auth/drive.readonly',
